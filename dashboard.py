42a48f41839498f511b5801521680a6dd64f346f
srihari dinesh <162956487+Hari-Dinesh@users.noreply.github.com>
2024-09-11 01:16:27 +0530

    fix: update env

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,7 @@ if os.getenv("DB_URL"):
 else:
     # Use Streamlit secrets (for Streamlit Cloud)
     mongo_url = st.secrets["db_url"]
-client = pymongo.MongoClient(mongo_url)
+client = pymongo.MongoClient("mongodb+srv://dinesh:Asdfg123&()@cluster0.5nxca.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client["task_manager"]
 goals_collection = db['goals']
 users_collection = db["users"]
--- task_manager.py
+++ task_manager.py
@@ -13,7 +13,7 @@ if os.getenv("DB_URL"):
 else:
     # Use Streamlit secrets (for Streamlit Cloud)
     mongo_url = st.secrets["db_url"]
-client = pymongo.MongoClient(mongo_url)
+client = pymongo.MongoClient("mongodb+srv://dinesh:Asdfg123&()@cluster0.5nxca.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client["task_manager"]
 tasks_collection = db["tasks"]
 users_collection = db["users"]
